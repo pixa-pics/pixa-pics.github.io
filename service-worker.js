72b842b575ee6ed144b0fa2f15f351f9e9618308
Viper <contact@vipertech.ch>
2022-03-17 14:29:52 +0100

    Swipeableview fix

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var REQUIRED_CACHE = "unless-update-cache-v39-required";
-var USEFUL_CACHE = "unless-update-cache-v39-useful";
-var STATIC_CACHE = "unless-update-cache-v39-static";
+var REQUIRED_CACHE = "unless-update-cache-v40-required";
+var USEFUL_CACHE = "unless-update-cache-v40-useful";
+var STATIC_CACHE = "unless-update-cache-v40-static";
 var CHILD_CHUNK_REGEX = /child\-chunk\.(main\~[a-z0-9]+|[0-9]+)\.min.js/i;
 
 // On install, cache some resource.
