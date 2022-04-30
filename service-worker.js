9f3b3903f2ae5a565344d0eb21c14d351299d0cb
Viper <contact@vipertech.ch>
2022-04-30 23:20:39 +0200

    Default color quant. on smartphone change

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var REQUIRED_CACHE = "unless-update-cache-v80-required";
-var USEFUL_CACHE = "unless-update-cache-v80-useful";
-var STATIC_CACHE = "unless-update-cache-v80-static";
+var REQUIRED_CACHE = "unless-update-cache-v81-required";
+var USEFUL_CACHE = "unless-update-cache-v81-useful";
+var STATIC_CACHE = "unless-update-cache-v81-static";
 var MAIN_CHILD_CHUNK_REGEX = /child\-chunk\.(main\~[a-z0-9]+)\.min.js/i;
 var CHILD_CHUNK_REGEX = /child\-chunk\.([0-9]+)\.min.js/i;
 
--- src/js/pages/Pixel.js
+++ src/js/pages/Pixel.js
@@ -844,15 +844,15 @@ class Pixel extends React.Component {
             const max_original_size = is_mobile_or_tablet ? Math.sqrt(1920 * 1080): Math.sqrt(4096 * 2160);
             const max_original_color = 1/0;
             const max_size = is_mobile_or_tablet ? Math.sqrt(1280 * 720): Math.sqrt(1920 * 1280);
-            const max_color = is_mobile_or_tablet ? 1024: 1536;
+            const max_color = is_mobile_or_tablet ? 768: 1536;
 
-            let ratio_l_l2 = is_mobile_or_tablet ? 2: 3;
+            let ratio_l_l2 = is_mobile_or_tablet ? 1.5: 3;
             let min_size = is_mobile_or_tablet ? 0: 0;
-            let min_color = is_mobile_or_tablet ? 512: 768;
+            let min_color = is_mobile_or_tablet ? 384: 768;
 
             const resize_original_to = max_original_size * max_original_size;
-            const resize_to = Math.min(max_size * max_size, Math.max(parseInt(_import_size) * parseInt(_import_size), min_size * min_size));
-            const limit_color_number = Math.min(max_color, Math.max(parseInt(_import_size) * ratio_l_l2, min_color));
+            const resize_to = Math.min(max_size * max_size, Math.max(parseInt(_import_size * _import_size), min_size * min_size));
+            const limit_color_number = Math.min(max_color, Math.max(parseInt(_import_size * ratio_l_l2), min_color));
 
             import("../utils/rgb_quant").then(({rgb_quant}) => {
 
