96abf12022afef76b47a39d49ec3055715cc9927
Viper <contact@vipertech.ch>
2022-05-10 05:08:01 +0200

    Used to not define which browser are to trans-pile JS for
    edge: "17",
                                            firefox: "60",
                                            chrome: "67",
                                            safari: "11.1"

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,20 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         query: {
-                            presets: ['react', 'env', 'stage-0']
+                            presets: [
+                                'react',
+                                ["env", {
+                                    targets: {
+                                        edge: "17",
+                                        firefox: "60",
+                                        chrome: "67",
+                                        safari: "11.1"
+                                    },
+                                    useBuiltIns: "usage",
+                                    forceAllTransforms: true,
+                                }],
+                                'stage-0'
+                            ]
                         }
                     }
                 ]
