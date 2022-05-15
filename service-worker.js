c876a7068ec2ad318d6b1ced2efb4a8a66ac4bf0
Viper <contact@vipertech.ch>
2022-05-15 10:44:20 +0200

    Chrome & Edge being supports  Illustration gets better text and reasons
    They feel stronger in their role being to take the Jamy emoji, further another environment to make his laboratory working perfectly

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var REQUIRED_CACHE = "unless-update-cache-v135-required";
-var USEFUL_CACHE = "unless-update-cache-v135-useful";
-var STATIC_CACHE = "unless-update-cache-v135-static";
+var REQUIRED_CACHE = "unless-update-cache-v136-required";
+var USEFUL_CACHE = "unless-update-cache-v136-useful";
+var STATIC_CACHE = "unless-update-cache-v136-static";
 var MAIN_CHILD_CHUNK_REGEX = /child\-chunk\.(main\~[a-z0-9]+)\.min.js/i;
 var CHILD_CHUNK_REGEX = /child\-chunk\.([0-9]+)\.min.js/i;
 
