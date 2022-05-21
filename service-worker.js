43cc6365d86fc98335a71828f1099e3339bebfd8
Viper <contact@vipertech.ch>
2022-05-22 01:18:55 +0200

    Added special chinese texture to toolbar

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var REQUIRED_CACHE = "unless-update-cache-v161-required";
-var USEFUL_CACHE = "unless-update-cache-v161-useful";
-var STATIC_CACHE = "unless-update-cache-v161-static";
+var REQUIRED_CACHE = "unless-update-cache-v162-required";
+var USEFUL_CACHE = "unless-update-cache-v162-useful";
+var STATIC_CACHE = "unless-update-cache-v162-static";
 var MAIN_CHILD_CHUNK_REGEX = /child\-chunk\.(main\~[a-z0-9]+)\.min.js/i;
 var CHILD_CHUNK_REGEX = /child\-chunk\.([0-9]+)\.min.js/i;
 
