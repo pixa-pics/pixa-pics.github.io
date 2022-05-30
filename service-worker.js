afdadffae5b7ecb95fefb8b3e81c8658f8cfd68e
Viper <contact@vipertech.ch>
2022-05-30 03:39:17 +0200

    T&est opti loading c

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var REQUIRED_CACHE = "unless-update-cache-v185-required";
-var USEFUL_CACHE = "unless-update-cache-v185-useful";
-var STATIC_CACHE = "unless-update-cache-v185-static";
+var REQUIRED_CACHE = "unless-update-cache-v186-required";
+var USEFUL_CACHE = "unless-update-cache-v186-useful";
+var STATIC_CACHE = "unless-update-cache-v186-static";
 var MAIN_CHILD_CHUNK_REGEX = /child\-chunk\.(main\~[a-z0-9]+)\.min.js/i;
 var CHILD_CHUNK_REGEX = /child\-chunk\.([0-9]+)\.min.js/i;
 
