3899ef1ed044f0f042f3f55157455abe0af77497
Viper <contact@vipertech.ch>
2022-06-12 00:20:31 +0200

    Quick fix of master file load

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var REQUIRED_CACHE = "unless-update-cache-v236-required";
-var USEFUL_CACHE = "unless-update-cache-v236-useful";
-var STATIC_CACHE = "unless-update-cache-v236-static";
+var REQUIRED_CACHE = "unless-update-cache-v237-required";
+var USEFUL_CACHE = "unless-update-cache-v237-useful";
+var STATIC_CACHE = "unless-update-cache-v237-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main\~[a-z0-9]+)\.min\.js/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js/i;
 
