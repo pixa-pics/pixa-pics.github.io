e21dbb1a40c924c4c12a4647cf17f68bce708d98
Viper <contact@vipertech.ch>
2022-06-26 20:00:40 +0200

    Omniscale + xBRZ fix

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var REQUIRED_CACHE = "unless-update-cache-v242-required";
-var USEFUL_CACHE = "unless-update-cache-v242-useful";
-var STATIC_CACHE = "unless-update-cache-v242-static";
+var REQUIRED_CACHE = "unless-update-cache-v243-required";
+var USEFUL_CACHE = "unless-update-cache-v243-useful";
+var STATIC_CACHE = "unless-update-cache-v243-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main\~[a-z0-9]+)\.min\.js/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js/i;
 
