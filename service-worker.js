e1d57d1ef6839d8e4b472abb3bedaf20c5a39a15
Viper <contact@vipertech.ch>
2022-07-31 23:16:14 +0200

    SW Version update

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var REQUIRED_CACHE = "unless-update-cache-v319-required";
-var USEFUL_CACHE = "unless-update-cache-v319-useful";
-var STATIC_CACHE = "unless-update-cache-v319-static";
+var REQUIRED_CACHE = "unless-update-cache-v320-required";
+var USEFUL_CACHE = "unless-update-cache-v320-useful";
+var STATIC_CACHE = "unless-update-cache-v320-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js/i;
 
