fde29e3230674b72e423988f4adec6cf4f34c1fd
Viper <contact@vipertech.ch>
2022-08-25 05:00:39 +0200

    Rndr system update

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v351-required";
-var USEFUL_CACHE = "unless-update-cache-v351-useful";
-var STATIC_CACHE = "unless-update-cache-v351-static";
+var REQUIRED_CACHE = "unless-update-cache-v352-required";
+var USEFUL_CACHE = "unless-update-cache-v352-useful";
+var STATIC_CACHE = "unless-update-cache-v352-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js/i;
 
