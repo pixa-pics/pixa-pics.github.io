fc50caea06b1ca765cffd61759f4bafdf28e73d1
Viper <contact@vipertech.ch>
2022-09-14 21:04:23 +0200

    Hompage bg

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v369-required";
-var USEFUL_CACHE = "unless-update-cache-v369-useful";
-var STATIC_CACHE = "unless-update-cache-v369-static";
+var REQUIRED_CACHE = "unless-update-cache-v370-required";
+var USEFUL_CACHE = "unless-update-cache-v370-useful";
+var STATIC_CACHE = "unless-update-cache-v370-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js/i;
 
