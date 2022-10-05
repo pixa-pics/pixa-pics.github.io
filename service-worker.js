44d1258e18af62e521ff67e86f21f61788ca0d63
Viper <contact@vipertech.ch>
2022-10-05 21:01:55 +0200

    SW update for filter update

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v417-required";
-var USEFUL_CACHE = "unless-update-cache-v417-useful";
-var STATIC_CACHE = "unless-update-cache-v417-static";
+var REQUIRED_CACHE = "unless-update-cache-v418-required";
+var USEFUL_CACHE = "unless-update-cache-v418-useful";
+var STATIC_CACHE = "unless-update-cache-v418-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js/i;
 
