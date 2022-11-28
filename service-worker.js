5eeef2c15b31e4f6124a502fea05ce85853716b3
Affolter Matias <pixa.pics@protonmail.com>
2022-11-28 23:18:41 +0100

    Reduce palette, a little bit of optimizations

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v534-required";
-var USEFUL_CACHE = "unless-update-cache-v534-useful";
-var STATIC_CACHE = "unless-update-cache-v534-static";
+var REQUIRED_CACHE = "unless-update-cache-v535-required";
+var USEFUL_CACHE = "unless-update-cache-v535-useful";
+var STATIC_CACHE = "unless-update-cache-v535-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
