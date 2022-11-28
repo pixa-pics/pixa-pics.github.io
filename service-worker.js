66d1e660ab68f2435f3404968d52d5880f6bd292
Affolter Matias <pixa.pics@protonmail.com>
2022-11-28 11:40:57 +0100

    Service worker usage for quant. +readme 1st image

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v532-required";
-var USEFUL_CACHE = "unless-update-cache-v532-useful";
-var STATIC_CACHE = "unless-update-cache-v532-static";
+var REQUIRED_CACHE = "unless-update-cache-v533-required";
+var USEFUL_CACHE = "unless-update-cache-v533-useful";
+var STATIC_CACHE = "unless-update-cache-v533-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
