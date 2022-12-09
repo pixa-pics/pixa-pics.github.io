2030cb74b1d96eeffe655687401aed07b34e59d7
Affolter Matias <pixa.pics@protonmail.com>
2022-12-09 13:26:22 +0100

    QuantiMat Changed clustering numbering parameters

--- src/js/utils/QuantiMat.js
+++ src/js/utils/QuantiMat.js
@@ -1,3 +1,28 @@
+"use strict";
+/*
+The MIT License (MIT)
+
+Copyright (c) 2022 - 2022 Matias Affolter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
 var QuantiMatGlobal = function(
     image_data,
     bucket_threshold,
@@ -518,7 +543,7 @@ var QuantiMatGlobal = function(
     Object.defineProperty(QuantiMat.prototype, 'reset_cluster', {
         get: function() { "use strict"; return function() {
             "use strict";
-            this.max_cluster_ = this.new_pxl_colors_.length > 2048 ? 4096+1: this.new_pxl_colors_.length > 1024 ? 256+1: this.new_pxl_colors_.length > 512 ? 16+1: 1;
+            this.max_cluster_ = this.new_pxl_colors_.length > 4096 ? 4096+1: this.new_pxl_colors_.length > 2048 ? 256+1: this.new_pxl_colors_.length > 1024 ? 16+1: 1;
             this.length_clusters_.fill(0, 0, this.max_cluster);
             for(var c = 0; (c|0) < (this.max_cluster|0); c=(c+1|0)>>>0){ this.index_clusters_[c|0] = [];}
         }}
