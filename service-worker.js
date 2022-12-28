39ed053db6cb79e87dbdcce5027eee3d8aeb5870
Affolter Matias <pixa.pics@protonmail.com>
2022-12-28 06:11:47 +0100

    QuantiMat improving results quality

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v574-required";
-var USEFUL_CACHE = "unless-update-cache-v574-useful";
-var STATIC_CACHE = "unless-update-cache-v574-static";
+var REQUIRED_CACHE = "unless-update-cache-v575-required";
+var USEFUL_CACHE = "unless-update-cache-v575-useful";
+var STATIC_CACHE = "unless-update-cache-v575-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
