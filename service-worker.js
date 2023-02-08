915b66fc001961bd7774d8c90c0bddf2bcbeb624
Affolter Matias <pixa.pics@protonmail.com>
2023-02-08 21:56:11 +0100

    presentation video got shorter

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v622-required";
-var USEFUL_CACHE = "unless-update-cache-v622-useful";
-var STATIC_CACHE = "unless-update-cache-v622-static";
+var REQUIRED_CACHE = "unless-update-cache-v623-required";
+var USEFUL_CACHE = "unless-update-cache-v623-useful";
+var STATIC_CACHE = "unless-update-cache-v623-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
