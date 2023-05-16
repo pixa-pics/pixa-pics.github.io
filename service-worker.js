338cc6c5b369ad700bc01f33f82b6a91c7c50e24
Affolter Matias <pixa.pics@protonmail.com>
2023-05-16 12:53:26 +0200

    Ghne

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v687-required";
-var USEFUL_CACHE = "unless-update-cache-v687-useful";
-var STATIC_CACHE = "unless-update-cache-v687-static";
+var REQUIRED_CACHE = "unless-update-cache-v688-required";
+var USEFUL_CACHE = "unless-update-cache-v688-useful";
+var STATIC_CACHE = "unless-update-cache-v688-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
