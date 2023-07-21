6f4b0b9a289adb8bdabb4d285b3d606642bfa281
Affolter Matias <pixa.pics@protonmail.com>
2023-07-21 12:51:58 +0200

    Improved sponsor image

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v814-required";
-var USEFUL_CACHE = "unless-update-cache-v814-useful";
-var STATIC_CACHE = "unless-update-cache-v814-static";
+var REQUIRED_CACHE = "unless-update-cache-v815-required";
+var USEFUL_CACHE = "unless-update-cache-v815-useful";
+var STATIC_CACHE = "unless-update-cache-v815-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
