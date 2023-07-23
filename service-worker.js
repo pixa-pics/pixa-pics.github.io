ab9f5067f13edc580cbca41fa3478b97de2ddc6a
Affolter Matias <pixa.pics@protonmail.com>
2023-07-23 07:40:10 +0200

    SW update

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v818-required";
-var USEFUL_CACHE = "unless-update-cache-v818-useful";
-var STATIC_CACHE = "unless-update-cache-v818-static";
+var REQUIRED_CACHE = "unless-update-cache-v819-required";
+var USEFUL_CACHE = "unless-update-cache-v819-useful";
+var STATIC_CACHE = "unless-update-cache-v819-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
