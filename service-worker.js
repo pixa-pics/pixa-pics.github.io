48e5a110bff251274c7c2763f61de250e0df29b4
Affolter Matias <pixa.pics@protonmail.com>
2023-10-04 21:35:32 +0200

    OXIPNG (WASM was in b64 now is stored in b92)

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v843-required";
-var USEFUL_CACHE = "unless-update-cache-v843-useful";
-var STATIC_CACHE = "unless-update-cache-v843-static";
+var REQUIRED_CACHE = "unless-update-cache-v844-required";
+var USEFUL_CACHE = "unless-update-cache-v844-useful";
+var STATIC_CACHE = "unless-update-cache-v844-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
