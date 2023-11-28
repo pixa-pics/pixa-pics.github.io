ebfd5566915597fe217939b14909e01280c50757
Affolter Matias <pixa.pics@protonmail.com>
2023-11-28 09:03:06 +0100

    Added an image in demo

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v861-required";
-var USEFUL_CACHE = "unless-update-cache-v861-useful";
-var STATIC_CACHE = "unless-update-cache-v861-static";
+var REQUIRED_CACHE = "unless-update-cache-v862-required";
+var USEFUL_CACHE = "unless-update-cache-v862-useful";
+var STATIC_CACHE = "unless-update-cache-v862-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
