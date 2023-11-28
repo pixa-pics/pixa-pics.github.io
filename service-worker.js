bf6599ea406593e85c2df30921e61615df01e711
Affolter Matias <pixa.pics@protonmail.com>
2023-11-28 18:02:26 +0100

    Addeda Pixel art

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v863-required";
-var USEFUL_CACHE = "unless-update-cache-v863-useful";
-var STATIC_CACHE = "unless-update-cache-v863-static";
+var REQUIRED_CACHE = "unless-update-cache-v864-required";
+var USEFUL_CACHE = "unless-update-cache-v864-useful";
+var STATIC_CACHE = "unless-update-cache-v864-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
