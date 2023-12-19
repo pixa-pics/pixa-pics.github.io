cb946c78c726564bdc6e25469c4cde6a3596693f
Affolter Matias <pixa.pics@protonmail.com>
2023-12-19 16:49:46 +0100

    B64 chromium module

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v889-required";
-var USEFUL_CACHE = "unless-update-cache-v889-useful";
-var STATIC_CACHE = "unless-update-cache-v889-static";
+var REQUIRED_CACHE = "unless-update-cache-v890-required";
+var USEFUL_CACHE = "unless-update-cache-v890-useful";
+var STATIC_CACHE = "unless-update-cache-v890-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
--- src/js/utils/b64.js
+++ src/js/utils/b64.js
@@ -1,5 +1,5 @@
-import {B64} from "chromium-base64";
-var b64 = new B64();
+import {B64chromium} from "chromium-base64";
+var b64 = new B64chromium();
 module.exports = {
     bytesToBase64: b64.bytesToBase64.bind(b64),
     base64ToBytes: b64.base64ToBytes.bind(b64)
