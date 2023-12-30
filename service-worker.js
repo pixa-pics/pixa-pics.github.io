28485cbb60937847dafd79407c4a7be607725d4e
Affolter Matias <pixa.pics@protonmail.com>
2023-12-30 08:45:06 +0100

    Added nice pixel art to the demo

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v902-required";
-var USEFUL_CACHE = "unless-update-cache-v902-useful";
-var STATIC_CACHE = "unless-update-cache-v902-static";
+var REQUIRED_CACHE = "unless-update-cache-v903-required";
+var USEFUL_CACHE = "unless-update-cache-v903-useful";
+var STATIC_CACHE = "unless-update-cache-v903-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
