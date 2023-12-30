d4462cde6c302630c57a8b086a10387d7652d661
Affolter Matias <pixa.pics@protonmail.com>
2023-12-30 10:25:57 +0100

    b64

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v903-required";
-var USEFUL_CACHE = "unless-update-cache-v903-useful";
-var STATIC_CACHE = "unless-update-cache-v903-static";
+var REQUIRED_CACHE = "unless-update-cache-v904-required";
+var USEFUL_CACHE = "unless-update-cache-v904-useful";
+var STATIC_CACHE = "unless-update-cache-v904-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
