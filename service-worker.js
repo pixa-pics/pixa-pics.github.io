4ca3a7a82d3c20b1b02098138c24e78405da5e26
Affolter Matias <pixa.pics@protonmail.com>
2024-01-16 02:04:20 +0100

    SW

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-var REQUIRED_CACHE = "unless-update-cache-v910-required";
-var USEFUL_CACHE = "unless-update-cache-v910-useful";
-var STATIC_CACHE = "unless-update-cache-v910-static";
+var REQUIRED_CACHE = "unless-update-cache-v911-required";
+var USEFUL_CACHE = "unless-update-cache-v911-useful";
+var STATIC_CACHE = "unless-update-cache-v911-static";
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
 var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
 
@@ -47,17 +47,20 @@ var serve_cache = function (cache_origin, url){
         "use strict";
         return cache.match(url).then(function (response) {
             "use strict";
-            return !response ? Promise.reject(): response.status === 200 ? response.clone(): fetch(url).then(function (response) { // Fetch, clone, and serve
+            return !response ? Promise.reject(): response.status === 200 ? response.clone()  || response: fetch(url).then(function (response2) { // Fetch, clone, and serve
                 "use strict";
-                if(response.status === 200) { cache.put(url, response.clone()); return Promise.resolve(response.clone()); } else { return Promise.reject(); }
+                if(response2.status === 200) { cache.put(url, response2.clone()); return Promise.resolve(response2.clone() || response2); } else { return Promise.reject(); }
             });
         }).catch(function(){
             "use strict";
-            return fetch(url).then(function (response) { // Fetch, clone, and serve
+            return fetch(url).then(function (response2) { // Fetch, clone, and serve
                 "use strict";
-                if(response.status === 200) { cache.put(url, response.clone()); return Promise.resolve(response.clone()); } else { return Promise.reject(); }
+                if(response2.status === 200) { cache.put(url, response2.clone()); return Promise.resolve(response2.clone() || response2); } else { return Promise.reject(); }
             });
         });
+    }).catch(function (){
+
+        return fetch(url);
     })
 };
 
@@ -88,7 +91,7 @@ var F_VID = function (n){return `/src/videos/${n}.mp4`;};
 var INSTALL_FILES_REQUIRED = ["/client/chunk_main_5a2dc592.min.js","/client/chunk_main_7a2ee6b6.min.js","/client/chunk_main_8afe242f.min.js","/client/chunk_main_253ae210.min.js","/client/chunk_main_678f84af.min.js","/client/chunk_main_690b702c.min.js","/client/chunk_main_748942c6.min.js","/client/chunk_main_af9f4ef7.min.js","/client/chunk_main_d939e436.min.js","/client/chunk_main_df0f15aa.min.js", "/client/chunk_norris.min.js", "/"];
 var INSTALL_FILES_USEFUL = ["/src/images/favicon.ico", "/src/images/manifest/logo-white.png", "/src/fonts/industry/index.css"];
 var LOAD_FILES_REQUIRED = ["/"].concat(new Array(45).map(F_CNK));
-var LOAD_FILES_USEFUL = ["/src/fonts/normative/index.css"].concat(["illusion.jpg", "adaragon.png", "gallery/Ban.png", "gallery/Ban.svg", "gallery/sco.png", "gallery/Sco.svg", "gallery/Bud.png", "gallery/Bud.svg", "demo/Beast.jpg", "demo/Disney.jpg", "demo/Man.jpg", "demo/Masterc.jpg", "demo/Redstar.jpg",  "demo/Tower.jpg", "infographics/Rambo.svg", "infographics/TestBag.svg", "infographics/Explosion.svg", "labostration/ABDUCTION.svg", "labostration/CHEMISTRY.svg", "labostration/COMPUTING.svg", "labostration/DOWNLOAD.svg", "labostration/GENOMA.svg", "labostration/MOLECULE.svg", "labostration/NUCLEAR.svg", "labostration/SCIENCE.svg", "REMINDER.svg", "Error.svg", "laboratory.svg", "illustrations/Camo.svg", "league/Bronze.png", "league/Diamond.png", "league/Gold.png", "league/Silver.png"].map(F_IMG));
+var LOAD_FILES_USEFUL = ["/src/fonts/normative/index.css"].concat(["illusion.jpg", "gallery/Ban.png", "gallery/Ban.svg", "gallery/sco.png", "gallery/Sco.svg", "gallery/Bud.png", "gallery/Bud.svg", "demo/Beast.jpg", "demo/Disney.jpg", "demo/Man.jpg", "demo/Masterc.jpg", "demo/Redstar.jpg",  "demo/Tower.jpg", "infographics/Rambo.svg", "infographics/TestBag.svg", "infographics/Explosion.svg", "labostration/ABDUCTION.svg", "labostration/CHEMISTRY.svg", "labostration/COMPUTING.svg", "labostration/DOWNLOAD.svg", "labostration/GENOMA.svg", "labostration/MOLECULE.svg", "labostration/NUCLEAR.svg", "labostration/SCIENCE.svg", "REMINDER.svg", "Error.svg", "laboratory.svg", "illustrations/Camo.svg", "league/Bronze.png", "league/Diamond.png", "league/Gold.png", "league/Silver.png"].map(F_IMG));
 var LOAD_FILES_STATIC = ["sfx/md/hero_decorative-celebration-02", "sfx/md/navigation_selection-complete-celebration", "sfx/md/navigation_transition-left", "sfx/md/state-change_confirm-down", "sfx/md/ui_lock", "sfx/md/ui_unlock", "sfx/md/ui_scan", "sfx/md/alert_high-intensity", "sfx/md/navigation_transition-right", "voice/cn/accessing_memory", "voice/cn/complete", "voice/cn/please_wait", "voice/cn/data_upload", "voice/cn/processing", "voice/cn/enhanced", "voice/cn/rewriting_deep_layer_protocols", "voice/cn/vision_activated", "voice/cn/vision_deactivated", "voice/cn/filtering", "music/redeclipse/track_09"].map(F_SND).concat(["presentation", "tutorial", "create", "enhanced", "pixelated", "upload", "share1", "joke1", "create", "enhanced", "pixelated", "presentation", "presentation2", "sponsors", "tutorial", "upload", "labintro", "share2", "share3", "share4", "share5", "share6", "share7", "joke2", "joke3", "joke4", "joke5", "joke6", "joke7", "joke8", "joke9", "joke10", "joke11"].map(F_VID));
 
 // On install, cache some resource.
@@ -177,22 +180,22 @@ self.addEventListener("fetch", function(event) {
             Promise.any([
                 required_cache.then(function (cache) {
                     return cache.match(url).then(function (response) {
-                        return !response ? Promise.reject(): response.status === 200 ? Promise.resolve(response.clone()): Promise.reject();
+                        return !response ? Promise.reject(): response.status === 200 ? Promise.resolve(response.clone() || response): Promise.reject();
                     });
                 }),
                 useful_cache.then(function (cache) {
                     return cache.match(url).then(function (response) {
-                        return !response ? Promise.reject(): response.status === 200 ? Promise.resolve(response.clone()): Promise.reject();
+                        return !response ? Promise.reject(): response.status === 200 ? Promise.resolve(response.clone() || response): Promise.reject();
                     });
                 }),
                 static_cache.then(function (cache) {
                     return cache.match(url).then(function (response) {
-                        return !response ? Promise.reject(): response.status === 200 ? Promise.resolve(response.clone()): Promise.reject();
+                        return !response ? Promise.reject(): response.status === 200 ? Promise.resolve(response.clone() || response): Promise.reject();
                     });
                 }),
                 fetch(url).then(function (response) { // Fetch and serve
                     return useful_cache.then(function (cache) {
-                        if(response.status === 200) { cache.put(url, response.clone()); return Promise.resolve(response.clone()); } else { return Promise.reject(); }
+                        if(response.status === 200) { cache.put(url, response.clone()); return Promise.resolve(response.clone() || response); } else { return Promise.reject(); }
                     });
                 })
             ])
@@ -215,86 +218,3 @@ self.addEventListener("activate", function(event) {
         )})
     );
 });
-
-"use strict";
-var REQUIRED_CACHE = "unless-update-cache-v907-required";
-var USEFUL_CACHE = "unless-update-cache-v907-useful";
-var STATIC_CACHE = "unless-update-cache-v907-static";
-var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-z0-9]+)\.min\.js$/i;
-var CHILD_CHUNK_REGEX = /chunk_([0-9]+)\.min\.js$/i;
-
-var required_cache_object = {};
-var required_cache = new Promise(function(resolve, reject){
-    "use strict";
-    if(typeof required_cache_object.addAll !== "undefined") {
-
-        resolve(required_cache_object);
-    }else {
-
-        caches.open(REQUIRED_CACHE).then(function(cache){required_cache_object = cache;resolve(required_cache_object);}).catch(function(reason){reject(reason)});
-    }
-});
-
-var useful_cache_object = {};
-var useful_cache = new Promise(function(resolve, reject){
-    "use strict";
-    if(typeof useful_cache_object.addAll !== "undefined") {
-
-        resolve(useful_cache_object);
-    }else {
-
-        caches.open(USEFUL_CACHE).then(function(cache){useful_cache_object = cache;resolve(useful_cache_object);}).catch(function(reason){reject(reason)});
-    }
-});
-
-var static_cache_object = {};
-var static_cache = new Promise(function(resolve, reject){
-    "use strict";
-    if(typeof static_cache_object.addAll !== "undefined") {
-
-        resolve(static_cache_object);
-    }else {
-
-        caches.open(STATIC_CACHE).then(function(cache){static_cache_object = cache;resolve(static_cache_object);}).catch(function(reason){reject(reason)});
-    }
-});
-
-var serve_cache = function (cache_origin, url){
-    "use strict";
-    return cache_origin.then(function (c) {
-        var cache = c;
-        "use strict";
-        return cache.match(url).then(function (response) {
-            "use strict";
-            return !response ? Promise.reject(): response.status === 200 ? response.clone(): fetch(url).then(function (response) { // Fetch, clone, and serve
-                "use strict";
-                if(response.status === 200) { cache.put(url, response); return Promise.resolve(response.clone()); } else { return Promise.reject(); }
-            });
-        }).catch(function(){
-            "use strict";
-            return fetch(url).then(function (response) { // Fetch, clone, and serve
-                "use strict";
-                if(response.status === 200) { cache.put(url); return Promise.resolve(response.clone()); } else { return Promise.reject(); }
-            });
-        });
-    })
-};
-
-var either_ends_with = function (possibilities, onto){
-    "use strict";
-    var result = false;
-    possibilities.forEach(function (possibility){
-        "use strict";
-        if(onto.endsWith(possibility)){result = true;}
-    });
-    return result;
-};
-var either_starts_with = function (possibilities, onto){
-    "use strict";
-    var result = false;
-    possibilities.forEach(function (possibility){
-        "use strict";
-        if(onto.startsWith(possibility)){result = true;}
-    });
-    return result;
-};
