403b4b98ef062aa08950fbc718e7785dd4c145a9
Affolter Matias <pixa.pics@protonmail.com>
2024-01-21 05:17:55 +0100

    Marketplace update image added

--- src/js/utils/meta-tags.js
+++ src/js/utils/meta-tags.js
@@ -1,7 +1,11 @@
 function update_meta_title(title) {
 
-    const meta_title = document.getElementById("meta-title");
-
+    let meta_title = document.getElementById("meta-title");
+    if(!Boolean(meta_title)){
+        meta_title = document.createElement("title");
+        meta_title.setAttribute("id", "meta-title");
+        document.head.append(meta_title);
+    }
     while( meta_title.firstChild ) {
 
         meta_title.removeChild( meta_title.firstChild );
